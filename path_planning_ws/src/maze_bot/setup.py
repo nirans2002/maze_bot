dd3c2e518bece679f72017e9fc46a40251d456f0
Niran S Narayanan <nirans2002@gmail.com>
2023-02-04 22:32:00 +0530

    meshes import

--- path_planning_ws/src/maze_bot/setup.py
+++ path_planning_ws/src/maze_bot/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+from glob import glob
+import os
 
 package_name = 'maze_bot'
 
@@ -10,7 +12,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share',package_name,'urdf'),glob('urdf/*')),
     ],
+
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='niran',
